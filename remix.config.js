84e9f424b57d70e94ce55999da0637295401e9ec
lumarioxonline <lumarioxonline@gmail.com>
2026-02-26 16:42:15 +0100

    Update remix.config.js

--- remix.config.js
+++ remix.config.js
@@ -1,5 +1,5 @@
 /** @type {import('@remix-run/dev').AppConfig} */
-module.exports = {
+export default {
   serverBuildPath: "build/server.js",
   serverDependenciesToBundle: [/^@shopify\/polaris/],
 };
