e7c5ca87550bff97e7fcc26f239fddc7b6069059
lumarioxonline <lumarioxonline@gmail.com>
2026-02-26 21:41:32 +0100

    Create remix.config.js

---
+++ remix.config.js
@@ -0,0 +1,6 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+export default {
+  serverBuildPath: "build/server.js",
+  serverModuleFormat: "esm",
+  serverDependenciesToBundle: [/^@shopify\/polaris/],
+};
