80bb9f5f47a891ffe3b9467084b944ddcfff6be9
lumarioxonline <lumarioxonline@gmail.com>
2026-02-26 16:45:36 +0100

    Create shopify.server.ts

---
+++ app/shopify.server.ts
@@ -0,0 +1,12 @@
+import { shopifyApp } from "@shopify/shopify-app-remix/server";
+import { MemorySessionStorage } from "@shopify/shopify-app-session-storage-memory";
+
+export const shopify = shopifyApp({
+  apiKey: process.env.SHOPIFY_API_KEY!,
+  apiSecretKey: process.env.SHOPIFY_API_SECRET!,
+  appUrl: process.env.SHOPIFY_APP_URL!,
+  scopes: (process.env.SCOPES || "").split(",").map((s) => s.trim()).filter(Boolean),
+  sessionStorage: new MemorySessionStorage(),
+});
+
+export const authenticate = shopify.authenticate;
