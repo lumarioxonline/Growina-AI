b05d3d752e7c00bc3dbb9c1ee80671011d66a791
lumarioxonline <lumarioxonline@gmail.com>
2026-02-26 17:35:29 +0100

    Update shopify.server.ts

--- app/shopify.server.ts
+++ app/shopify.server.ts
@@ -1,12 +1,18 @@
+import { PrismaClient } from "@prisma/client";
+import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
 import { shopifyApp } from "@shopify/shopify-app-remix/server";
-import { MemorySessionStorage } from "@shopify/shopify-app-session-storage-memory";
+
+const prisma = new PrismaClient();
 
 export const shopify = shopifyApp({
   apiKey: process.env.SHOPIFY_API_KEY!,
   apiSecretKey: process.env.SHOPIFY_API_SECRET!,
   appUrl: process.env.SHOPIFY_APP_URL!,
-  scopes: (process.env.SCOPES || "").split(",").map((s) => s.trim()).filter(Boolean),
-  sessionStorage: new MemorySessionStorage(),
+  scopes: (process.env.SCOPES || "")
+    .split(",")
+    .map((s) => s.trim())
+    .filter(Boolean),
+  sessionStorage: new PrismaSessionStorage(prisma),
 });
 
 export const authenticate = shopify.authenticate;
