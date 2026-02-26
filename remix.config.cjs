31f953082ae4e24ccc725fb04c7c7da0d18e552c
lumarioxonline <lumarioxonline@gmail.com>
2026-02-26 21:41:04 +0100

    Delete remix.config.cjs

--- remix.config.cjs
+++
@@ -1,6 +0,0 @@
-/** @type {import('@remix-run/dev').AppConfig} */
-export default {
-  serverBuildPath: "build/server.js",
-  serverModuleFormat: "esm",
-  serverDependenciesToBundle: [/^@shopify\/polaris/]
-};
